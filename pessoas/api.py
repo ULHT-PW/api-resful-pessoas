53d6c8a7bf5361dafcb81cc80fcde505405713d0
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2025-07-07 13:23:04 +0000

    restruturação

--- pessoas/api.py
+++ pessoas/api.py
@@ -1,44 +1,18 @@
+
 from django.shortcuts import get_object_or_404
 from ninja import NinjaAPI
 from .models import Pessoa
 from .schemas import PessoaIn, PessoaOut
 from typing import List
 
-api = NinjaAPI()
+api = NinjaAPI(
+    title="API das Pessoas",
+    description="API para gerir pessoas",
+    version="1.0.0",
+)
+
 
 # LISTAR
 @api.get("pessoas/", response=List[PessoaOut])
 def listar_pessoas(request):
     return Pessoa.objects.all()
-
-
-# VER UMA
-@api.get("pessoas/{id}/", response=PessoaOut)
-def ver_uma_pessoa(request, id: int):
-    pessoa = get_object_or_404(Pessoa, id=id)
-    return pessoa
-
-
-# CRIAR
-@api.post("pessoas/", response=PessoaOut)
-def criar_uma_pessoa(request, data: PessoaIn):
-    pessoa = Pessoa.objects.create(**data.dict())
-    return 201, pessoa
-
-
-# ATUALIZAR
-@api.put("pessoas/{id}/", response=PessoaOut)
-def atualizar_uma_pessoa(request, id: int, data: PessoaIn):
-    pessoa = get_object_or_404(Pessoa, id=id)
-    for attr, value in data.dict().items():
-        setattr(pessoa, attr, value)
-    pessoa.save()
-    return pessoa
-
-
-# APAGAR
-@api.delete("pessoas/{id}/")
-def apagar_uma_pessoa(request, id: int):
-    pessoa = get_object_or_404(Pessoa, id=id)
-    pessoa.delete()
-    return 204, None
--- pessoas/schemas.py
+++ pessoas/schemas.py
@@ -1,13 +1,13 @@
 from ninja import Schema
 
 class PessoaIn(Schema):
-    nome: str 
-    idade: int
-
-class PessoaOut(Schema):
-    id: int
     nome: str
     idade: int
-    
+
+
+class PessoaOut(PessoaIn):
+    id: int
+
+
 class ErrorSchema(Schema):
     detail: str
